ec0a9f3d093b08ffe8cf3717a109cbb3c53309a0
AlisovTimofey <timofeyalisov@gmail.com>
2026-01-23 09:16:41 +0300

    class Shape

--- alisov.timofey/P5/main.cpp
+++ alisov.timofey/P5/main.cpp
@@ -1,4 +1,22 @@
+#include <cmath>
 #include <iostream>
+#include <new>
+#include <stdexcept>
+namespace alisov
+{
+  class shape
+  {
+  public:
+    virtual ~Shape() noexcept = default;
+    virtual double getArea() const noexcept = 0;
+    virtual rectangle_t getFrameRect() const noexcept = 0;
+    virtual void move(point_t c) noexcept = 0;
+    virtual void move(double d_x, double d_y) noexcept = 0;
+
+    void scaleWithCheck(double coef);
+    virtual void Scale(double coef) noexcept = 0;
+  };
+}
 
 int main()
 {
